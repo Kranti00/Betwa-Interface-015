f43a337b244a84effbf6adbf721083cfe718dcd4
Kranti Kumar <krantikumar@Krantis-MacBook-Air.local>
2024-05-08 14:02:07 +0530

    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Kranti section
+
+
+// Kranti section end
+
+
+
+
+
+//Haresh section
+
+
+// Haresh section end
+
+
+
+
+
+// Soumyadeep section
+
+
+// Soumyadeep section end
+
+
+
+
+
+// Adarsh section
+
+
+// Adarsh section end
