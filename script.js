6b42f4ee625abdcb6e5319771e2b1e2f8653bd4c
Kranti Kumar <krantikumar@Krantis-MacBook-Air.local>
2024-05-09 01:02:31 +0530

    navbar done

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Kranti section
+let poplocation = document.getElementById("kpopuplocation");
+let locationdiv = document.getElementById("kjs_location");
+let crossmark = document.getElementById("kjs_xmark");
 
+locationdiv.addEventListener("click", function(){
+    poplocation.style.display = "block";
+})
+
+crossmark.addEventListener("click", function(){
+    poplocation.style.display = "none";
+})
 
 // Kranti section end
 
